ac5fabb2d6d624af264fc3fac91eee17cb843e4a
Gtps.Finance <welcome@gtps.finance>
2022-11-19 00:06:30 +0000

    Update index.tsx

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -714,8 +714,7 @@ export default function Swap() {
                           ) : (
                             <MouseoverTooltip
                               text={
-                                <Trans>
-                                  You must give Gtps.Finance permission to use your{' '}
+                                <Trans>You must give Gtps.Finance permission to use your{' '}
                                   {currencies[Field.INPUT]?.symbol}. You only have to do this once per token.
                                 </Trans>
                               }
